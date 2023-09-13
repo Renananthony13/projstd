abde0f1fbd3999ce6211f615064e2065a2a87896
Renananthony13 <renanalmassa17@gmail.com>
2023-09-13 10:20:56 -0300

    segundo commit middlewares

--- aula06(MysqlNode)/src/middlewares/auth.js
+++ aula06(MysqlNode)/src/middlewares/auth.js
@@ -1,6 +1,7 @@
 // middleware de autenticacao
 
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config()
 const { promisify } = require('util');
 
 module.exports = async (req, res, next) => {
@@ -8,9 +9,23 @@ module.exports = async (req, res, next) => {
 
     if(token) {
         res.status(401).send('Access token nao informado')
+    } 
+
+    const [text, token_ver] = token.split(' ')
+
+    try {
+
+        jwt.verify(token_ver, process.env.SECRET_KEY)
+
+        const {iduser, emailUser} = jwt.decode(token_ver)
+        
+        req.iduser = iduser
+        req.emailUser = emailUser
+
+        return next()
+    } catch(error) {
+        res.status(501).res('Acess toke n')
     }
-
-
     
 }
 
